173e1299443406def31fb3c35df2077306599c82
ItelSunday <iteldomingotech@gmail.com>
2019-06-13 13:24:40 -0700

    merge sort using recursion

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,50 +20,6 @@ arr = [9, 7, 8, 5, 6, 1, 3, 4, 2, 0 ]
 selection_sort(arr)
 print(arr)       
         
-        
-# def selection_sort(arr):
-#     # loop through n-1 elements
-#         for i in range(0, len(arr) - 1):
-#            smallest_index = arr[i]
-#         #    print(arr)
-#         for j in range(i+1, len(arr)):
-#             if arr[i] > arr[j]:
-#                 arr[i], arr[j] = arr[j], arr[i]
-#             print(arr)    
-            # return(arr)
-           
-# selection_sort(arr)
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-        
-    
-    
-    
-    
-    # for i in range(0, len(arr) - 1):
-    #     cur_index = i
-    #     smallest_index = cur_index
-    #     for j in range(cur_index, len(arr)):
-    #       if arr[smallest_index] > arr[j]:
-    
-    #     # TO-DO: find next smallest element
-    #     # (hint, can do in 3 loc) 
-
-    # while cur_index > 0 and smallest_index < l[cur_index - 1]:
-    #     print(l)
-    #     l(cur_index) = l[cur_index-1]
-    #     cur_index -= 1
-
-
-    #     smallest_index = cur_index
-    #     smallest_index = l[i]
-    #     cur_index = i
-        # TO-DO: swap
-
-
-
-
-#     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
