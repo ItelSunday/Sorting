1fd8710650b716d9c4d0fb839ccf1d9472d041ec
ItelSunday <iteldomingotech@gmail.com>
2019-06-17 17:44:03 -0700

    wip: binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,8 +2,17 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
-
-   return -1   # not found
+  arr = [1, 3, 50, 15, 23, 11, 8, 2, 118]
+  x = 10
+  len = len(arr)
+  
+  
+  def find(arr, length, x):
+    for i in range (0, len):
+      if (arr[i] == x):
+        return i
+      
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
